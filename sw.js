773e54b0b313debea122479e207d95709bc924b0
ilcagnonelabrador <ilcagnonelabrador@gmail.com>
2026-04-04 14:54:43 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'scout-over-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // API calls sempre dalla rete
+  if (e.request.url.includes('api-sports.io') || e.request.url.includes('fonts.googleapis')) {
+    e.respondWith(fetch(e.request).catch(() => new Response('', { status: 503 })));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
